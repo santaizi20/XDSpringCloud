5fcec9cd00fd51978c59743040e4b3314f0b533a
mac <429033084@qq.com>
2019-04-15 16:04:14 +0800

    xd

--- EurekaServer/XDEurekaServer/src/main/java/com/xd/Start.java
+++ EurekaServer/XDEurekaServer/src/main/java/com/xd/Start.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
 @SpringBootApplication
-public class Start {
+public class Start {111
 	public static void main(String[] args) {
 		SpringApplication.run(Start.class);
 	}
